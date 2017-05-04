76983ac8827ba841c9e14fbf0d679ebae2c5f80f
Uurbas <ugurbas91@gmail.com>
2017-05-04 22:13:18 +0300

    Add files via upload

---
+++ Receiver.ino
@@ -0,0 +1,116 @@
+#include <SPI.h>
+#include "RF24.h"
+//SCK -> 13//MISO -> 12//MOSI -> 11//CSN -> 7//CE -> 8
+RF24 radio(8, 7);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int msg[2];
+int in1 = A0;
+int in2 = A1;
+int in3 = A2;
+int in4 = A3;
+int enA = 3;
+int enB = 5;
+void setup()
+{
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openReadingPipe(1, pipe);
+  radio.startListening();
+}
+void loop()
+{
+  if (radio.available()) {
+    radio.read(msg, sizeof(msg));
+    //
+    if (0 <= msg[0] && msg[0] <= 18)//forward part
+    {
+      digitalWrite(enA, HIGH);
+      digitalWrite(enB, HIGH);
+      delay(40 - msg[0]);
+      digitalWrite(enA, LOW);
+      digitalWrite(enB, LOW);
+      delay(msg[0]);
+      digitalWrite(in1, HIGH);
+      digitalWrite(in3, HIGH);
+      digitalWrite(in2, LOW);
+      digitalWrite(in4, LOW);
+
+      if (msg[1] <= 40 && 22 <= msg[1])//turn right
+      {
+        digitalWrite(enA, HIGH);
+        //digitalWrite(enB, HIGH);
+        delay(msg[1]);
+        digitalWrite(enA, LOW);
+        digitalWrite(enB, LOW);
+        delay(40 - msg[1]);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in4, LOW);
+      }
+      if (0 <= msg[1] && msg[1] <= 18)//turn left
+      {
+        //digitalWrite(enA, HIGH);
+        digitalWrite(enB, HIGH);
+        delay(40 - msg[1]);
+        digitalWrite(enA, LOW);
+        digitalWrite(enB, LOW);
+        delay(msg[1]);
+        digitalWrite(in1, HIGH);
+        digitalWrite(in3, HIGH);
+        digitalWrite(in2, LOW);
+        digitalWrite(in4, LOW);
+      }
+    }
+    if (msg[0] <= 40 && 22 <= msg[0])//backward part
+    {
+      digitalWrite(enA, HIGH);
+      digitalWrite(enB, HIGH);
+      delay(msg[0]);
+      digitalWrite(enA, LOW);
+      digitalWrite(enB, LOW);
+      delay(40 - msg[0]);
+      digitalWrite(in2, HIGH);
+      digitalWrite(in4, HIGH);
+      digitalWrite(in1, LOW);
+      digitalWrite(in3, LOW);
+      if (msg[1] <= 40 && 22 <= msg[1])//turn right
+      {
+        digitalWrite(enA, HIGH);
+        //digitalWrite(enB, HIGH);
+        delay(msg[1]);
+        digitalWrite(enA, LOW);
+        digitalWrite(enB, LOW);
+        delay(40 - msg[1]);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in4, HIGH);
+        digitalWrite(in1, LOW);
+        digitalWrite(in3, LOW);
+      }
+      if (0 <= msg[1] && msg[1] <= 18)//turn left
+      {
+        //digitalWrite(enA, HIGH);
+        digitalWrite(enB, HIGH);
+        delay(40 - msg[1]);
+        digitalWrite(enA, LOW);
+        digitalWrite(enB, LOW);
+        delay(msg[1]);
+        digitalWrite(in2, HIGH);
+        digitalWrite(in4, HIGH);
+        digitalWrite(in1, LOW);
+        digitalWrite(in3, LOW);
+      }
+    }
+    //    Serial.print("msg0 : ");
+    //    Serial.println(msg[0]);
+    //    Serial.print("msg1 : ");
+    //    Serial.println(msg[1]);
+    //delay(100);
+  }
+}
---
+++ Transmitter.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+#include "RF24.h"
+int msg[2];
+//SCK -> 13//MISO -> 12//MOSI -> 11//CSN -> 7//CE -> 8
+RF24 radio(8, 7);
+const uint64_t pipe = 0xE8E8F0F0E1LL;
+int potpin1 = A0;
+int potpin2 = A1;
+int val1;
+int val2;
+
+int Alcoholpin = A2;
+uint8_t AlcoSpeed;
+
+void setup(void) {
+  //Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(pipe);
+  AlcoSpeed=0;
+}
+void loop(void) {
+  AlcoSpeed = (analogRead(Alcoholpin)>>2);
+  //Serial.print(AlcoSpeed);
+  //Serial.print(",");
+  delay(2);
+  if (AlcoSpeed<32)
+  {
+    val1 = analogRead(potpin1);
+    val1 = map(val1, 0, 1023, 0, 40);
+    val2 = analogRead(potpin2);
+    val2 = map(val2, 0, 1023, 0, 40);
+    delay(1);
+    msg[0] = val1;
+    msg[1] = val2;
+    radio.write(msg, sizeof(msg));
+    //Serial.print(val1);
+    //Serial.print(",");
+    //Serial.println(val2);
+
+  }
+  else
+  {
+    msg[0] = 20;
+    msg[1] = 20;
+    radio.write(msg, sizeof(msg));
+  }
+
+}
